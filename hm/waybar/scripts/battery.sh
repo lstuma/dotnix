f2ae90addba203ad8066a4e6396ee1a55694485d
lstuma <lstuma@lstuma.dev>
2025-01-29 17:30:21 +0100

    184 current  2025-01-29 17:16:36  25.05pre-git   6.12.11  2043731d7537b976cca63cfaf1e643478beabc39  *

--- hm/waybar/scripts/battery.sh
+++ hm/waybar/scripts/battery.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-echo -e "Hello World! BATTERIES!!!"
 
+BATTERY=$(battery)
+CHARGING=$(echo "$BATTERY" | grep "Charging")
+PERCENTAGE=$(echo "$BATTERY" | grep -Eo "[0-9]+")
+
+if [[ $CHARGING ]];
+then
+  echo -en "<span class=\"battery-charge-icon\">󱐋 </span>"
+fi
+
+echo -ne "<span class=\"battery-charge-number\">$PERCENTAGE<span class=\"batter-charge-percent\">%</span></span>"
