36072158d6b84cbd11518d836cb3238de7b102ff
lstuma <lstuma@lstuma.dev>
2025-01-29 16:17:03 +0100

    154 current  2025-01-29 16:12:42  25.05pre-git   6.12.11  6ff24f46be000f0e1d85a47f5ad4cc82354cb8e6  *

--- hm/tmux/default.nix
+++ hm/tmux/default.nix
@@ -7,6 +7,27 @@
   programs.tmux = {
     enable = true;
     plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.tmux-nova;
+        extraConfig = ''
+          # tmux-nova
+          set -g @nova-nerdfonts true
+          set -g @nova-nerdfonts-left 
+          set -g @nova-nerdfonts-right 
+
+          set -g @nova-segment-mode "#{?client_prefix,+,*}"
+          set -g @nova-segment-mode-colors "#50fa7b #000000"
+
+          set -g @nova-segment-whoami "#(whoami)@#h"
+          set -g @nova-segment-whoami-colors "#50fa7b #282a36"
+
+          set -g @nova-pane "#I#{?pane_in_mode, > #{pane_mode},} #W"
+
+          set -g @nova-rows 0
+          set -g @nova-segments-0-left "mode"
+          set -g @nova-segments-0-right "whoami"
+        '';
+      }
       tmuxPlugins.tmux-nova
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.prefix-highlight
@@ -15,23 +36,6 @@
     extraConfig = ''
       # enable mouse click
       set -g mouse on
-
-      # tmux-nova
-      set -g @nova-nerdfonts true
-      set -g @nova-nerdfonts-left 
-      set -g @nova-nerdfonts-right 
-
-      set -g @nova-segment-mode "#{?client_prefix,+,*}"
-      set -g @nova-segment-mode-colors "#50fa7b #000000"
-
-      set -g @nova-segment-whoami "#(whoami)@#h"
-      set -g @nova-segment-whoami-colors "#50fa7b #282a36"
-
-      set -g @nova-pane "#I#{?pane_in_mode, > #{pane_mode},} #W"
-
-      set -g @nova-rows 0
-      set -g @nova-segments-0-left "mode"
-      set -g @nova-segments-0-right "whoami"
 
       # tab switching
       bind-key -r C-Right next-window
