7650c577b47d9d14d190e3b3095ace564fd8274d
lstuma <lstuma@lstuma.dev>
2025-01-29 17:50:34 +0100

    198 current  2025-01-29 17:49:58  25.05pre-git   6.12.11  d543c016e5467c36534942efe5c0cd22d8db49ac  *

--- hm/waybar/scripts/battery.sh
+++ hm/waybar/scripts/battery.sh
@@ -4,7 +4,7 @@ BATTERY=$(battery)
 CHARGING=$(echo "$BATTERY" | grep "Charging")
 PERCENTAGE=$(echo "$BATTERY" | grep -Eo "[0-9]+")
 
-OUTPUT="<spann color=\"#ff7a93\">"
+OUTPUT="<span color=\"#ff7a93\">"
 if [[ $CHARGING ]];
 then
   OUTPUT="<span color=\"#7da6ff\">󱐋 "
