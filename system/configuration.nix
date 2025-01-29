c3edf2bc12eb2b713f3551355b5a4eaf14853104
lstuma <lstuma@lstuma.dev>
2025-01-29 18:06:48 +0100

    206 current  2025-01-29 17:57:41  25.05pre-git   6.12.11  99aae75d842516f8c9a95366fba420390316ebbb  *

--- system/configuration.nix
+++ system/configuration.nix
@@ -85,13 +85,19 @@
 
   # rtkit is optional but recommended
   security.rtkit.enable = true;
-  services.pipewire = {
+  #services.pipewire = {
+    #enable = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
+    #pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  #};
+  services.pulseaudio = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
   };
 
   # Allow unfree packages
