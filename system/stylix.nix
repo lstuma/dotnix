9b9c29398540e88d2998f6486e8875a78c4bc7c2
lstuma <lstuma@lstuma.dev>
2025-01-30 09:04:21 +0100

    219 current  2025-01-30 08:53:57  25.05pre-git   6.12.11  71cbce67ec456edffae32d997f345aefe9a582ee  *

--- system/stylix.nix
+++ system/stylix.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [ base16-schemes ];
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     image = ./wallpaper.png;
     cursor = {
       name = "phinger-cursors-light";
