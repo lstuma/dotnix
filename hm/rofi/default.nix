037890d4e7de6299db3e73972aba6e0e562de2fb
lstuma <lstuma@lstuma.dev>
2025-01-30 12:41:53 +0100

    279 current  2025-01-30 12:41:37  25.05pre-git   6.12.11  c0f6e4436d10b556bcc79c82bd9539e09a5c6d18  *

--- hm/rofi/default.nix
+++ hm/rofi/default.nix
@@ -82,11 +82,12 @@
         border-radius = mkLiteral "10px";
       };
       "#element" = {
-        padding = mkLiteral "5px 5px";
+        padding = mkLiteral "5px";
         border = mkLiteral "0px solid";
         border-radius = mkLiteral "5px";
       };
       "#element-icon" = {
+        padding = mkLiteral "5px";
         size = mkLiteral "24px";
       };
       "#element-text" = {
