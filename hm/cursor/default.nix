5e7b7df43f1df73d7ebdad44bb0e31ac37151683
lstuma <lstuma@lstuma.dev>
2025-01-30 08:43:58 +0100

    217 current  2025-01-30 08:31:51  25.05pre-git   6.12.11  311b4782d695a207c98654d2d2831c2692d6aa32  *

--- hm/cursor/default.nix
+++ hm/cursor/default.nix
@@ -3,11 +3,11 @@
   home.pointerCursor = {
     name = "phinger-cursors-light";
     package = pkgs.phinger-cursors;
-    size = 32;
+    size = 16;
     gtk.enable = true;
   };
 
-  wayland.windowManager.hyprland.exec-once = [
-    "hyprctl setcursor phinger-cursors-light 24"
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "hyprctl setcursor phinger-cursors-light 12"
   ];
 }
