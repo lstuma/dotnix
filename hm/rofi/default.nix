cf82194a019443358be89d43acd19e4d1d3577ef
lstuma <lstuma@lstuma.dev>
2025-01-30 11:56:20 +0100

    250 current  2025-01-30 11:53:33  25.05pre-git   6.12.11  d1ad54432a6f98cc3f6a7bd9701e01eb10ffd01c  *

--- hm/rofi/default.nix
+++ hm/rofi/default.nix
@@ -7,6 +7,7 @@
 
     theme = {
       "#window" = {
+        enabled = true;
         location = "center";
         anchor = "center";
         width = "600px";
@@ -16,11 +17,13 @@
         border-radius = "10px";
       };
       "#mainbox" = {
+        enabled = true;
         spacing = "10px";
         margin = "0px";
         padding = "30px";
       };
       "#inputbar" = {
+        enabled = true;
         spacing = "10px";
         margin = "0px";
         padding = "0px";
@@ -28,28 +31,35 @@
         border-radius = "0px";
       };
       "#prompt" = { 
+        enabled = true;
       };
       "#textbox-prompt-colon" = {
+        enabled = true;
         padding = "5px";
         expand = false;
         str = "";
       };
       "#entry" = {
+        enabled = true;
         padding = "5px 0px";
         placeholder = "Search...";
         placeholder-colon = "inherit";
       };
       "#num-filtered-rows" = {
-
+        enabled = true;
+        expand = false;
       };
       "#textbox-num-sep" = {
-      
+        enabled = true;
+        expand = false; 
+        str = "/";
       };
       "#num-rows" = {
-
+        enabled = true;
+        expand = false;
       };
       "#case-indicator" = {
-
+        enabled = true;
       };
       "#listview" = {
 
