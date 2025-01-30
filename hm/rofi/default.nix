1176042f00d0fe600257c63744941d62b6f466ae
lstuma <lstuma@lstuma.dev>
2025-01-30 11:45:22 +0100

    247 current  2025-01-30 11:27:32  25.05pre-git   6.12.11  1058987b0139d303db0b05d8c44f2c43792e6403  *

--- hm/rofi/default.nix
+++ hm/rofi/default.nix
@@ -2,5 +2,59 @@
 {
   programs.rofi = {
     enable = true;
+    cycle = true;
+    location = "center";
+
+    theme = {
+      "#window" = {
+        location = "center";
+        anchor = "center";
+        width = "600px";
+        margin = "0px";
+        padding = "0px";
+        border = "0px solid";
+        border-radius = "10px";
+      };
+      "#mainbox" = {
+        spacing = "10px";
+        margin = "0px";
+      };
+      "#inputbar" = {
+        spacing = "10px";
+        margin = "0px";
+        padding = "0px";
+      };
+      "#prompt" = {
+
+      };
+      "#textbox-prompt-colon" = {
+
+      };
+      "#entry" = {
+
+      };
+      "#num-filtered-rows" = {
+
+      };
+      "#textbox-num-sep" = {
+      
+      };
+      "#num-rows" = {
+
+      };
+      "#case-indicator" = {
+
+      };
+      "#listview" = {
+
+      };
+      "#scrollbar" = {
+
+      };
+      "#element" = {
+
+      };
+
+    };
   };
 }
