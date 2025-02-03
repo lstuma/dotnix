3d55214c9393ee26653d1c1d7cc3821d6ed81709
lstuma <lstuma@lstuma.dev>
2025-02-03 18:59:09 +0100

    367 current  2025-02-03 18:49:17  25.05pre-git   6.12.11  5a31fc468fcad5a6d57184640cc0891a4218db25  *

--- hm/waybar/scripts/shared.sh
+++ hm/waybar/scripts/shared.sh
@@ -7,17 +7,23 @@ output() {
     echo "{\"text\":\"$text\", \"tooltip\": \"$tooltip\"}"
 }
 
-SLEEPWATCH_TIMESTEP=0.2
 sleepwatch() {
+    # check if file is deleted or created
     file=$1
-    time=$2
-    state=$(stat -c %Y "$file")
+    time=$((time * 10))
+    if [ ! -f "$file" ]; then
+        state="deleted"
+    else
+        state="created"
+    fi
     waited=0
     while [ $waited -lt $time ]; do
-        if [ $state -ne $(stat -c %Y "$file") ]; then
+        if [ ! -f "$file" ] && [ "$state" = "created" ]; then
+            break
+        elif [ -f "$file" ] && [ "$state" = "deleted" ]; then
             break
         fi
-        sleep $SLEEPWATCH_TIMESTEP
-        waited=$(($waited + $SLEEPWATCH_TIMESTEP))
+        sleep 0.1
+        waited=$(($waited + 1))
     done
 }
