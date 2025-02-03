56e475b9218cb02106f83424eb6274a8589e810d
lstuma <lstuma@lstuma.dev>
2025-02-03 22:21:03 +0100

    402 current  2025-02-03 22:15:35  25.05pre-git   6.12.11  de54527d66cd6fe2d19c6a73db770d18d5f6c50d  *

--- hm/nixos-wizard/default.nix
+++ hm/nixos-wizard/default.nix
@@ -8,10 +8,10 @@ let
 in
 pythonPackages.buildPythonPackage rec {
   pname = "nixos-wizard";
-  version = "0.1.0";
+  version = "0.1.1";
   src = pkgs.fetchurl {
-    url = "https://files.pythonhosted.org/packages/13/01/1ab54acdafece3d9951a4bee4204ff1cca4c7a8ec02311af112e5b4242da/nixos_wizard-0.1.0.tar.gz";
-    sha256 = "7ad58437c6dc90904969697e40173f1da7a8f8324aa5b4576c8b71fe18e9b505";
+    url = "https://files.pythonhosted.org/packages/30/10/a44bdeaaa4169eb6448e18b29061d3cca8def8ae7ded99c70409acd603b1/nixos_wizard-0.1.1.tar.gz";
+    sha256 = "0aeb4caf8429d63d75c9021f28b0f5f51286c5bf4fa6388887488d3dc5dea495";
   };
   doCheck = false;
   propagatedBuildInputs = dependencies;
