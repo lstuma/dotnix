7751f13c5e4daae85a44a3ce65c0242edb505faa
lstuma <lstuma@lstuma.dev>
2025-02-03 16:42:44 +0100

    326 current  2025-02-03 16:30:04  25.05pre-git   6.12.11  193af35821f2ccf8fcc92da3d0bc9a90ed46fbf0  *

--- hm/waybar/scripts/battery.sh
+++ hm/waybar/scripts/battery.sh
@@ -1,21 +1,44 @@
 #!/usr/bin/env bash
 
+GREEN="#41d992"
+YELLOW="#f5ec6e"
+ORANGE="#ff9e64"
+RED="#ff7a93"
+PURPLE="#bb9af7"
+LIGHT_GREEN="#b9f27c"
+LIGHT_BLUE="#7da6ff"
+
 BATTERY=$(battery)
 CHARGING=$(echo "$BATTERY" | grep "Charging")
 FULL=$(echo "$BATTERY" | grep "Full")
-PERCENTAGE=$(echo "$BATTERY" | grep -Eo "[0-9]+")
+CHARGE=$(echo "$BATTERY" | grep -Eo "[0-9]+")
+COLOR="$RED"
+ICON="󱐋"
 
-if [[ $CHARGING ]];
-then
-  OUTPUT="<span color=\"#7da6ff\">󱐋"
-elif [[ $FULL ]];
-then
-  OUTPUT="<span color=\"#b9f27c\"> "
+
+if [[ $FULL ]] then
+    ICON=""
+    COLOR="$GREEN"
+elif [[ $CHARGING ]] then
+    ICON="󱐋"
+    COLOR="$LIGHT_BLUE"
 else
-  OUTPUT="<span color=\"#ff7a93\"> "
+    if [[ "$CHARGE" -lt "20" ]] then
+        ICON=""
+        COLOR="$RED"
+    elif [[ "$CHARGE" -lt "50" ]] then
+        ICON="󰁽"
+        COLOR="$ORANGE"
+    elif [[ "$CHARGE" -lt "80" ]] then
+        ICON="󰂀"
+        COLOR="$YELLOW"
+    else
+        ICON="󱈑"
+        COLOR="$LIGHT_GREEN"
+    fi
 fi
 
-OUTPUT+="$PERCENTAGE<span>%</span></span>"
+OUTPUT+="<span color=\"$COLOR\">$ICON $CHARGE%</span>"
 
 
 echo -ne "$OUTPUT"
