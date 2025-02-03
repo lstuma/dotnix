93242bc931438972e5a382bb3f13ec6596cb73be
lstuma <lstuma@lstuma.dev>
2025-02-03 22:11:52 +0100

    400 current  2025-02-03 21:56:52  25.05pre-git   6.12.11  56f20abfeb7dcdc1084e000805aec3771b61f3d8  *

--- hm/nixos-wizard/default.nix
+++ hm/nixos-wizard/default.nix
@@ -5,8 +5,8 @@ in
 pythonPackages.buildPythonPackage rec {
   pname = "nixos-wizard";
   version = "0.1.0";
-  src = pkgs.fetchPypi {
-    inherit pname version;
+  src = pkgs.fetchurl {
+    url = "https://files.pythonhosted.org/packages/13/01/1ab54acdafece3d9951a4bee4204ff1cca4c7a8ec02311af112e5b4242da/nixos-wizard/nixos-wizard-0.1.0.tar.gz";
     sha256 = "0sh0m1hgjrw55mx82hg52c65mlhp2y3nhpwacvfrpyvikgcgprk5";
   };
   doCheck = false;
