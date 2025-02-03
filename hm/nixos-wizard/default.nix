de54527d66cd6fe2d19c6a73db770d18d5f6c50d
lstuma <lstuma@lstuma.dev>
2025-02-03 22:15:16 +0100

    401 current  2025-02-03 22:14:09  25.05pre-git   6.12.11  5983f1586bd486102d904d76b3ec788c04b09acc  *

--- hm/nixos-wizard/default.nix
+++ hm/nixos-wizard/default.nix
@@ -1,6 +1,10 @@
 { pkgs }:
 let
   pythonPackages = pkgs.python3Packages;
+    dependencies = with pythonPackages; [
+        rich
+        pyyaml
+    ];
 in
 pythonPackages.buildPythonPackage rec {
   pname = "nixos-wizard";
@@ -10,4 +14,5 @@ pythonPackages.buildPythonPackage rec {
     sha256 = "7ad58437c6dc90904969697e40173f1da7a8f8324aa5b4576c8b71fe18e9b505";
   };
   doCheck = false;
+  propagatedBuildInputs = dependencies;
 }
