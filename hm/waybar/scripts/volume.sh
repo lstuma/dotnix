ae848756080b3b8647c9a560edf66567a4c7b8c6
lstuma <lstuma@lstuma.dev>
2025-02-03 16:23:37 +0100

    319 current  2025-02-03 15:27:14  25.05pre-git   6.12.11  c6d3426f1d243d6490b440bd00684cb15069721b  *

--- hm/waybar/scripts/volume.sh
+++ hm/waybar/scripts/volume.sh
@@ -4,25 +4,24 @@ RED="#8be9fd"
 PURPLE="#8be9fd"
 MAGENTA="#8be9fd"
 
-VOLUME=$(pamix --get-volume)
+VOLUME=$(pamixer --get-volume)
 COLOR="#bd93f9";
 
-if [[ $(pamixer --get-mute) ]] then
+if [[ "$(pamixer --get-mute)" =~ "true"  ]] then
   OUTPUT="󰝟 "
   COLOR="$RED"
 else
-  if (( "$VOLUME" -lt "0" )) then
+  if [[ "$VOLUME" -lt "0" ]] then
     COLOR="$MAGENTA"
     OUTPUT="󰕿 "
-  elif (( "$VOLUME" -lt "50")) then
+  elif [[ "$VOLUME" -lt "50" ]] then
     COLOR="$PURPLE"
     OUTPUT="󰖀 "
-  else 
+  else
     COLOR="$PURPLE"
     OUTPUT="󰕾 "
   fi
   OUTPUT+="$VOLUME"
 fi
 
-echo "$OUTPUT"
-
+echo "<span color=\"$COLOR\">$OUTPUT</span>"
