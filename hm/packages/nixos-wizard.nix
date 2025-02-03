c8b4ef379a47dbfff45128ac741f9dd36936ad47
lstuma <lstuma@lstuma.dev>
2025-02-03 22:31:36 +0100

    404 current  2025-02-03 22:28:20  25.05pre-git   6.12.11  8059cac863df0b72c5d948a11d5cd3dd2d7cd7e5  *

--- hm/packages/nixos-wizard.nix
+++ hm/packages/nixos-wizard.nix
@@ -1,4 +1,18 @@
-{ lib, config, pkgs, ... }:
-{
-
+{ pkgs }:
+let
+  pythonPackages = pkgs.python3Packages;
+    dependencies = with pythonPackages; [
+        rich
+        pyyaml
+    ];
+in
+pythonPackages.buildPythonPackage rec {
+  pname = "nixos-wizard";
+  version = "0.1.1";
+  src = pkgs.fetchurl {
+    url = "https://files.pythonhosted.org/packages/30/10/a44bdeaaa4169eb6448e18b29061d3cca8def8ae7ded99c70409acd603b1/nixos_wizard-0.1.1.tar.gz";
+    sha256 = "0aeb4caf8429d63d75c9021f28b0f5f51286c5bf4fa6388887488d3dc5dea495";
+  };
+  doCheck = false;
+  propagatedBuildInputs = dependencies;
 }
