d9e323616792f47879788edcafa70c520c333254
lstuma <lstuma@lstuma.dev>
2025-02-04 17:36:48 +0100

    426 current  2025-02-04 17:29:45  25.05pre-git   6.12.11  0d2878091a6d1920893267d14308ce8d3571e58d  *

--- hm/stylix/default.nix
+++ hm/stylix/default.nix
@@ -1,8 +1,17 @@
 { lib, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    papirus-icon-theme
+    breeze-icons
+    adwaita-icon-theme
+  ];
+
   stylix = {
     enable = true;
     polarity = "dark";
-    iconTheme = "breeze";
+    stylix.iconTheme = {
+      name = "breeze"; # You can change this to "Papirus", "Adwaita", etc.
+      package = pkgs.breeze-icons; # Ensure the package is installed
+    };
   };
 }
