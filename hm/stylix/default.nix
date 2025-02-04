1336db42ebe9e11ac77230c2d948ebfb9fc379e6
lstuma <lstuma@lstuma.dev>
2025-02-04 17:24:47 +0100

    425 current  2025-02-04 17:20:36  25.05pre-git   6.12.11  fa0bf9da49054e2aea272fc795fbdb4999d3552c  *

--- hm/stylix/default.nix
+++ hm/stylix/default.nix
@@ -1,5 +1,39 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    gradience
+  ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = lib.mkForce "adw-gtk3-dark";
+      package = lib.mkForce pkgs.adw-gtk3;
+    };
+    iconTheme.package = pkgs.papirus-icon-theme;
+    iconTheme.name = "Papirus";
+  
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+	    '';
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style.name = "adwaita-dark";
+    style.package = pkgs.adwaita-qt;
+  };   
+  home.sessionVariables.GTK_THEME = "adw-gtk3-dark";
+
   stylix = {
     enable = true;
     polarity = "dark";
