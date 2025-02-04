a3f14b4c2971eecbe8c43bd80eab9cc1a16505e5
lstuma <lstuma@lstuma.dev>
2025-02-04 17:35:37 +0100

    426 current  2025-02-04 17:29:45  25.05pre-git   6.12.11  0d2878091a6d1920893267d14308ce8d3571e58d  *

--- hm/stylix/default.nix
+++ hm/stylix/default.nix
@@ -3,10 +3,6 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    iconTheme = {
-      package = pkgs.papirus-icon-theme.override { color = "indigo"; };
-      dark = "Papirus-Dark"; # used
-      light = "Papirus-Light"; # unused
-    };
+    iconTheme = "breeze";
   };
 }
