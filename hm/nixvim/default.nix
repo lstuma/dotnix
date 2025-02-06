727f13d5177f01b07790fa5fa83a775ad4138950
lstuma <lstuma@lstuma.dev>
2025-02-06 12:44:31 +0100

    464 current  2025-02-06 12:07:10  25.05pre-git   6.12.11  18f5400d061e8c0b6a0a13781ab0f97c9cc7cc20  *

--- hm/nixvim/default.nix
+++ hm/nixvim/default.nix
@@ -90,6 +90,28 @@
           };
         };
       };
+      bufferline = {
+        enable = true;
+        settings = {
+          options = {
+            diagnostics = "nvim_lsp";
+            mode = "buffers";
+
+            close_icon = "󰅚 ";
+            buffer_close_icon = " ";
+            modified_icon =" ";
+            
+            offsets = [
+              {
+                filetype = "neo-tree";
+                text = "Neo-tree";
+                highlight = "Directory";
+                text_align = "left";
+              }
+            ];
+          };
+        };
+      };
     };
   };
 }
