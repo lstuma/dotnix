5a28ddc31c8ad0b4a0a8f004bcbc35ca4d553a88
lstuma <lstuma@lstuma.dev>
2025-02-06 12:02:12 +0100

    461 current  2025-02-06 11:49:42  25.05pre-git   6.12.11  d9472deb1861304c6e45ba1e8357be1b189a216d  *

--- hm/nixvim/default.nix
+++ hm/nixvim/default.nix
@@ -3,5 +3,55 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+    plugins = {
+      lsp = {
+        enable = true;
+        inlayHints = true;
+      };
+      treesitter = {
+        enable = true;
+        settings = {
+          indent.enable = true;
+          highlight.enable = true;
+        };
+        folding = false;
+        nixvimInjections = true;
+        grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+      };
+      illuminate = {
+        enable = true;
+        underCursor = false;
+        filetypesDenylist = [
+          "Outline"
+          "TelescopePrompt"
+          "alpha"
+          "harpoon"
+          "reason"
+        ];
+      };
+      neo-tree = {
+        enable = true;
+        sources = [
+          "filesystem"
+          "buffers"
+          "document_symbols"
+        ];
+        addBlankLineAtTop = false;
+        filesystem = {
+          bindToCwd = false;
+          followCurrentFile = {
+            enabled = true;
+          };
+        };
+        defaultComponentConfigs = {
+          indent = {
+            withExpanders = true;
+            expanderCollapsed = "󰅂";
+            expanderExpanded = "󰅀";
+            expanderHighlight = "NeoTreeExpander";
+          };
+        };
+      };
+    };
   };
 }
