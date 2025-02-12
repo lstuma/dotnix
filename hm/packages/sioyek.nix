329cedb824d033fda6f4dc2bb9d2e0eac7349d90
lstuma <lstuma@lstuma.dev>
2025-02-12 17:16:03 +0100

    517 current  2025-02-12 17:12:09  25.05pre-git   6.12.12  ddb58cab37d0d77be9da11816cdca1fb15231b48  *

--- hm/packages/sioyek.nix
+++ hm/packages/sioyek.nix
@@ -13,6 +13,7 @@ pythonPackages.buildPythonPackage rec {
     url = "https://files.pythonhosted.org/packages/07/c4/05ba9ae45384c4b6d1ed43bb370b4d38d442ae6f461d8bd9669d6efe14d6/sioyek-0.31.11.tar.gz";
     sha256 = "2b3af02b6d4253cd2b4f64ad9ecdc8ba5cb6fbcdf0e8c5cf51d4cb92abbd4be3";
   };
+  format = "pyproject";
   doCheck = false;
   propagatedBuildInputs = dependencies;
 }
