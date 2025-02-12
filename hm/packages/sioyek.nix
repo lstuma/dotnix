d24f3412f578a29dbebe7f817a2ac8375174da5a
lstuma <lstuma@lstuma.dev>
2025-02-12 17:13:44 +0100

    517 current  2025-02-12 17:12:09  25.05pre-git   6.12.12  ddb58cab37d0d77be9da11816cdca1fb15231b48  *

--- hm/packages/sioyek.nix
+++ hm/packages/sioyek.nix
@@ -10,8 +10,8 @@ pythonPackages.buildPythonPackage rec {
   pname = "nixos-wizard";
   version = "0.1.1";
   src = pkgs.fetchurl {
-    url = "https://files.pythonhosted.org/packages/30/10/a44bdeaaa4169eb6448e18b29061d3cca8def8ae7ded99c70409acd603b1/nixos_wizard-0.1.1.tar.gz";
-    sha256 = "0aeb4caf8429d63d75c9021f28b0f5f51286c5bf4fa6388887488d3dc5dea495";
+    url = "https://files.pythonhosted.org/packages/07/c4/05ba9ae45384c4b6d1ed43bb370b4d38d442ae6f461d8bd9669d6efe14d6/sioyek-0.31.11.tar.gz";
+    sha256 = "2b3af02b6d4253cd2b4f64ad9ecdc8ba5cb6fbcdf0e8c5cf51d4cb92abbd4be3";
   };
   doCheck = false;
   propagatedBuildInputs = dependencies;
