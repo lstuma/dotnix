e1674629e55d77cf454d20beb9f47317f47b18ad
lstuma <lstuma@lstuma.dev>
2025-02-14 17:14:38 +0100

    528 current  2025-02-14 17:13:23  25.05pre-git   6.12.12  792619842cd10911ae3aa142d4fbc2a1bbec3992  *

--- hm/waybar/scripts/ollama.sh
+++ hm/waybar/scripts/ollama.sh
@@ -6,12 +6,11 @@ get-status() {
     # get status of the service
     ollama_stat="$(systemctl is-active open-webui.service)"
     openwebui_stat="$(systemctl is-active open-webui.service)"
-    if [ ollama_stat -eq "active" ] && [ openwebui_stat -eq "active" ]; then
-        return "active"
-    elif [ ollama_stat -eq "inactive" ] && [ openwebui_stat -eq "inactive" ]; then
-        return "inactive"
+    if [ "$ollama_stat" = "$openwebui_stat" ]; then
+        echo "$ollama_stat"
+        return
     fi
-    return "unknown"
+    echo "unknown"
 }
 
 if [ "$1" = "click" ]; then
@@ -36,11 +35,11 @@ COLOR_ON="$GREEN"
 COLOR_UNKNOWN="$PURPLE"
 
 while true; do
-    status="$(get-status)"
-    if [ "$status" -eq "active" ]; then
+    ai_status="$(get-status)"
+    if [ "$ai_status" = "active" ]; then
         icon="$ICON_ON"
         color="$COLOR_ON"
-    elif [ "$status" -eq "inactive" ]; then
+    elif [ "$ai_status" = "inactive" ]; then
         icon="$ICON_OFF"
         color="$COLOR_OFF"
     else
